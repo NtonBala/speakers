c508bd42b1558e14e6db695fb54ea580399a6191
Anton Balashov <balashov.anton.1983@gmail.com>
2023-06-02 17:55:23 +0300

    Substitute useState with useReducer in one line of code

--- pages/demo-reducer.js
+++ pages/demo-reducer.js
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
 
 export default function Demo() {
-  const [cnt, setCnt] = useState(10);
+  // const [cnt, setCnt] = useState(10);
+  // * This code works exactly like useState did
+  const [cnt, setCnt] = useReducer((_, action) => action, 10);
 
   return <button onClick={() => setCnt(cnt + 1)}>{cnt}</button>;
 }
