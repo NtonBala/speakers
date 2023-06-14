d78b0d8c3dafa6d939e5f178db80806d5aca9dca
Anton Balashov <balashov.anton.1983@gmail.com>
2023-06-14 11:33:17 +0300

    Add useRef #1 use case demo

--- pages/demo-useref.js
+++ pages/demo-useref.js
@@ -1,7 +1,23 @@
+import { useRef } from 'react';
+
 export default function Demo() {
+  // useRef use case #1: reference DOM
+  const imgRef = useRef();
+
   return (
     <div className="container">
-      <img src="/images/Speaker-1124.jpg" alt="" />
+      <img
+        src="/images/Speaker-1124.jpg"
+        alt=""
+        ref={imgRef}
+        style={{ filter: 'grayscale(100%)' }}
+        onMouseOver={() => {
+          imgRef.current.style.filter = 'grayscale(0%)';
+        }}
+        onMouseOut={() => {
+          imgRef.current.style.filter = 'grayscale(100%)';
+        }}
+      />
       <hr />
       <button
         onClick={() => {
