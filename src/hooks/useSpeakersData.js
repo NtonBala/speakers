4befe7e57ccb63d1a36ef0da24fa9a4a3654a6e3
Anton Balashov <balashov.anton.1983@gmail.com>
2023-11-05 16:04:39 +0200

    Add useSpeakersData hook

---
+++ src/hooks/useSpeakersData.js
@@ -0,0 +1,31 @@
+import { useGeneralizedCrudMethods } from './useGeneralizedCrudMethods';
+
+export const useSpeakersData = () => {
+  const url = '/api/speakers';
+  const errorNotificationFn = (error) => {
+    console.log('Error From useSpeakersData', error);
+  };
+
+  const { data, error, loadingStatus, createRecord, updateRecord, deleteRecord } = useGeneralizedCrudMethods(url, errorNotificationFn);
+
+  function createSpeaker(speakerRec, callbackDone) {
+    createRecord(speakerRec, callbackDone);
+  }
+
+  function updateSpeaker(speakerRec, callbackDone) {
+    updateRecord(speakerRec, callbackDone);
+  }
+
+  function deleteSpeaker(id, callbackDone) {
+    deleteRecord(id, callbackDone);
+  }
+
+  return {
+    speakerList: data,
+    loadingStatus,
+    error,
+    createSpeaker,
+    updateSpeaker,
+    deleteSpeaker,
+  };
+};
