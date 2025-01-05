327cf2ad73d42dd8d79eaa178f4db8560ed256c7
Anton Balashov <balashov.anton.1983@gmail.com>
2025-01-05 17:28:03 +0200

    Refactor Speaker component to use SpeakersDataContext

--- src/components/speakers/Speaker.js
+++ src/components/speakers/Speaker.js
@@ -1,11 +1,14 @@
 import { useContext } from 'react';
 import SpeakerDetail from './SpeakerDetail';
-import { speakerList } from '../../../speakersData';
 import { ThemeContext } from '../../contexts/ThemeContext';
+import { SpeakersDataContext, SpeakersDataProvider } from '../../contexts/SpeakersDataContext';
 
-export default function Speaker({ id }) {
+function Inner({ id }) {
   const { darkTheme } = useContext(ThemeContext);
+  const { speakerList, loadingStatus } = useContext(SpeakersDataContext);
   const speakerRec = speakerList?.find((rec) => rec.id === id);
+
+  if (loadingStatus === 'loading') return <div>Loading...</div>;
 
   return speakerRec ? (
     <div className={darkTheme ? 'theme-dark' : 'theme-light'}>
@@ -13,5 +16,13 @@ export default function Speaker({ id }) {
     </div>
   ) : (
     <h2 className="text-danger">Speaker {id} not found</h2>
+  );
+}
+
+export default function Speaker({ id }) {
+  return (
+    <SpeakersDataProvider>
+      <Inner id={id} />
+    </SpeakersDataProvider>
   );
 }
